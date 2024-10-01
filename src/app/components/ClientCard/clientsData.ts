714bc571bf40fc6a735cc0ebcd7e97adf22b9359
Andrei Alexandrov <andrei.alxv@gmail.com>
2024-10-01 23:23:44 +0300

    New image positions in the about me galleries

--- src/app/components/ClientCard/clientsData.ts
+++ src/app/components/ClientCard/clientsData.ts
@@ -6,12 +6,12 @@ import jaklinTwo from "../../assets/people/jaklin2.jpg";
 import pollyaIvanova from "../../assets/people/polia.jpg";
 
 export const clientImages = [
-  { src: mihaelaFileva, name: "Mihaela Fileva" },
   { src: jaklinTarakci, name: "Jaklin Tarakci" },
-  { src: daraYotova, name: "Dara Yotova" },
   { src: jaklin, name: "Jaklin Tarakci" },
   { src: pollyaIvanova, name: "Pollya Ivanova" },
   { src: mihaelaFileva, name: "Mihaela Fileva" },
   { src: daraYotova, name: "Dara Yotova" },
   { src: jaklinTwo, name: "Jaklin Tarakci" },
+  { src: daraYotova, name: "Dara Yotova" },
+  { src: mihaelaFileva, name: "Mihaela Fileva" },
 ];
