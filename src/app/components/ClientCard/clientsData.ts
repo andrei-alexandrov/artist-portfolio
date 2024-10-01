b703caf4f821ba4972ab68616107b5865a94c7a1
Andrei Alexandrov <andrei.alxv@gmail.com>
2024-10-01 23:01:53 +0300

    Changed order of gallery pictures in About section

--- src/app/components/ClientCard/clientsData.ts
+++ src/app/components/ClientCard/clientsData.ts
@@ -6,10 +6,10 @@ import jaklinTwo from "../../assets/people/jaklin2.jpg";
 import pollyaIvanova from "../../assets/people/polia.jpg";
 
 export const clientImages = [
-  { src: jaklinTarakci, name: "Jaklin Tarakci" },
   { src: mihaelaFileva, name: "Mihaela Fileva" },
-  { src: jaklin, name: "Jaklin Tarakci" },
+  { src: jaklinTarakci, name: "Jaklin Tarakci" },
   { src: daraYotova, name: "Dara Yotova" },
+  { src: jaklin, name: "Jaklin Tarakci" },
   { src: pollyaIvanova, name: "Pollya Ivanova" },
   { src: mihaelaFileva, name: "Mihaela Fileva" },
   { src: daraYotova, name: "Dara Yotova" },
