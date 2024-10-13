44f4b88cc60460f746e6883a5741541916658ec4
Andrei Alexandrov <andrei.alexandrov@abv.bg>
2024-10-13 23:59:54 +0300

    Testing if the video playback issue is fixed on production

--- src/app/pages/courses/coursesData.ts
+++ src/app/pages/courses/coursesData.ts
@@ -1,14 +1,14 @@
 const coursesData = [
     {
-        videoUrl: "/public-assets/isi-1.mp4",
-        coverPhoto: "/public-assets/isi-2-pic.jpeg",
+        videoUrl: "/public-assets/isi-1.MP4",
+        coverPhoto: "/public-assets/isi-1-pic.JPEG",
         title: `"Онлайн курс "Аз, АРТИСТЪТ"`,
         description1: "Място за текст или можем да оставим само видеото с cover снимка, където ще пишем името на курса и датите на провеждане директно върху нея :)",
         description2: "А ако има нещо отдолу, може да е примерно някаква допълнителна информация за самия курс, ти какво мислиш?",
     },
     {
-        videoUrl: "/public-assets/isi-2.mp4",
-        coverPhoto: "/public-assets/isi-1-pic.jpeg",
+        videoUrl: "/public-assets/isi-2.MP4",
+        coverPhoto: "/public-assets/isi-2-pic.JPEG",
         title: "Въведение във вокалната техника",
         description1: "Място за текст или можем да оставим само видеото с cover снимка, където ще пишем името на курса и датите на провеждане директно върху нея :)",
         description2: "А ако има нещо отдолу, може да е примерно някаква допълнителна информация за самия курс, ти какво мислиш?",
