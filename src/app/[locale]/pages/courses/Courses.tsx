49e52a742f7201ec7ea51beb4a8cb2376f28f887
Andrei Alexandrov <andrei.alexandrov@abv.bg>
2024-10-21 15:15:16 +0300

    Fonts adjustments + overall minor adjustments

--- src/app/[locale]/pages/courses/Courses.tsx
+++ src/app/[locale]/pages/courses/Courses.tsx
@@ -1,17 +1,16 @@
 // Courses.tsx
 import { useTranslations } from "next-intl";
 import { getCoursesData } from "./coursesData";
+
 import "./Courses.scss";
 
 const Courses = () => {
   const t = useTranslations();
-
-  // Get the translated courses data
   const coursesData = getCoursesData(t);
 
   return (
     <>
-      <div id="courses" className="courses-container">{t("courses.title")}</div>
+      <div id="courses" className="courses-title">{t("courses.title")}</div>
       <div className="card-container">
         {coursesData.map((content, index) => (
           <div key={index} className="card">
@@ -21,7 +20,7 @@ const Courses = () => {
               playsInline
               poster={content.coverPhoto}
             ></video>
-            <h2>{content.title}</h2>
+            <div className="courses-title">{content.title}</div>
             <p className="description1">{content.description1}</p>
             <p className="description2">{content.description2}</p>
           </div>
